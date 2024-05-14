86ab6cb0596353f6d5e9c5bd33ff06bc2564abd7
vasylzayachkivskyi <60969456+vasylzayachkivskyi@users.noreply.github.com>
2024-05-14 10:57:39 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -76,24 +76,24 @@ $(document).ready(function () {
     });
 
     // input mask
-    $('.phone-input').mask('+38 (999)999-99-99', {
-        onBeforeMask: function (t, e) { return t.replace(/^38/g, '') },
-        onBeforePaste: function (t, e) { return t.replace(/^38/g, '') }
-    });
-    $.fn.setCursorPosition = function (pos) {
-        if ($(this).get(0).setSelectionRange) {
-            $(this).get(0).setSelectionRange(pos, pos);
-        } else if ($(this).get(0).createTextRange) {
-            var range = $(this).get(0).createTextRange();
-            range.collapse(true);
-            range.moveEnd('character', pos);
-            range.moveStart('character', pos);
-            range.select();
-        }
-    };
-    $('input[name="phone"]').click(function () {
-        $(this).setCursorPosition(5);
-    });
+    // $('.phone-input').mask('+38 (999)999-99-99', {
+    //     onBeforeMask: function (t, e) { return t.replace(/^38/g, '') },
+    //     onBeforePaste: function (t, e) { return t.replace(/^38/g, '') }
+    // });
+    // $.fn.setCursorPosition = function (pos) {
+    //     if ($(this).get(0).setSelectionRange) {
+    //         $(this).get(0).setSelectionRange(pos, pos);
+    //     } else if ($(this).get(0).createTextRange) {
+    //         var range = $(this).get(0).createTextRange();
+    //         range.collapse(true);
+    //         range.moveEnd('character', pos);
+    //         range.moveStart('character', pos);
+    //         range.select();
+    //     }
+    // };
+    // $('input[name="phone"]').click(function () {
+    //     $(this).setCursorPosition(5);
+    // });
 
 
 
